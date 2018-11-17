bec3be2bf6a4dddbf67c5420ac014e5521bcbbaf
ALEX <alextanase9000@gmail.com>
2018-11-17 23:32:22 +0200

    update version: citirea locatiei GPS

--- app/src/main/java/com/td/kira/lab1ex5newloginregister/GPSActivity.java
+++ app/src/main/java/com/td/kira/lab1ex5newloginregister/GPSActivity.java
@@ -1,10 +1,28 @@
 package com.td.kira.lab1ex5newloginregister;
 
+import android.Manifest;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class GPSActivity extends AppCompatActivity {
+
+    private Button b;
+    private TextView t;
+    private DatabaseReference GpsData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +30,27 @@ public class GPSActivity extends AppCompatActivity {
         setContentView(R.layout.activity_gps);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        GpsData = FirebaseDatabase.getInstance().getReference("GPS");
+        t = (TextView) findViewById(R.id.textField);
+        b = (Button) findViewById(R.id.button);
+
+        ActivityCompat.requestPermissions(GPSActivity.this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 123);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GpsTracker gt = new GpsTracker(getApplicationContext());
+                Location l = gt.getLocation();
+                if( l == null){
+                    Toast.makeText(getApplicationContext(),"GPS unable to get Value",Toast.LENGTH_SHORT).show();
+                }else {
+                    double lat = l.getLatitude();
+                    double lon = l.getLongitude();
+                    Toast.makeText(getApplicationContext(),"Lat = "+lat+"\n lon = "+lon,Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
