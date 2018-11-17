afaa097c00f6be5ab4d7e3bcb6223aa670dff6f9
ALEX <alextanase9000@gmail.com>
2018-11-17 21:24:44 +0200

    update version: buton de back la pagina principala

--- app/src/main/java/com/td/kira/lab1ex5newloginregister/GPSActivity.java
+++ app/src/main/java/com/td/kira/lab1ex5newloginregister/GPSActivity.java
@@ -2,6 +2,7 @@ package com.td.kira.lab1ex5newloginregister;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class GPSActivity extends AppCompatActivity {
 
@@ -9,5 +10,17 @@ public class GPSActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gps);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
